ccc155645acef7235dab8d95f413dfd74b0cacfa
Daniil Usmantsev <daniil.usmantsev@gmail.com>
2026-07-28 19:58:24 +0300

    add yandex metrika

--- src/widgets/app-hooks/yandex-metrika/yandex-metrika.tsx
+++ src/widgets/app-hooks/yandex-metrika/yandex-metrika.tsx
@@ -43,7 +43,7 @@ export const YandexMetrika = ({
               m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
               m[i].l=1*new Date();
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=${yandexTrackingId}", "ym");
 
             ym(${yandexTrackingId}, "init", ${JSON.stringify(options)});
           `,
