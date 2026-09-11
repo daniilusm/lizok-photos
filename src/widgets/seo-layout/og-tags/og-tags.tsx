d83cb2849e1d1a47f4d27289e3f6ee86fd0f3eab
Daniil Usmantsev <daniil.usmantsev@gmail.com>
2026-09-11 20:46:04 +0300

    add google search

--- src/widgets/seo-layout/og-tags/og-tags.tsx
+++ src/widgets/seo-layout/og-tags/og-tags.tsx
@@ -27,6 +27,10 @@ export const OgTags = (props: SeoLayoutDataType) => {
       <meta name="googlebot" content="index, follow, max-image-preview:large" />
       <meta name="yandex" content="index, follow" />
       <meta name="yandex-verification" content="5f7290056ff63009" />
+      <meta
+        name="google-site-verification"
+        content="BhkppVqJo9Au5-9rZuTRvbhKLVtnwvXGqBxnaAG2iHs"
+      />
       <meta name="theme-color" content={theme} />
       <meta name="color-scheme" content="light" />
       <meta name="format-detection" content="telephone=no" />
