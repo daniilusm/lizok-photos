85350f10c875d22ac0c884ab6c657b2f35195fe6
Daniil Usmantsev <daniil.usmantsev@gmail.com>
2026-09-15 11:13:18 +0300

    upd 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,23 @@
 import type { GetStaticProps } from "next";
+import Head from "next/head";
 
 import { NotFoundPage } from "@/_pages/not-found/ui";
 import { notFoundSeo, withCmsSeo } from "@/shared/seo";
 
 const Page = () => {
-  return <NotFoundPage />;
+  return (
+    <>
+      {/* If a host serves this HTML under a wrong URL, jump to the real route. */}
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(){var p=location.pathname;if(p!=="/404"&&p!=="/404/")location.replace("/404/");})();`,
+          }}
+        />
+      </Head>
+      <NotFoundPage />
+    </>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async () => ({
